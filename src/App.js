d231fb29c4ea32ec79a61354112d5f4f30ac0069
Tatiana Nikonova <taniushka_2007@mail.ru>
2022-06-20 20:40:32 +0300

    Json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, HashRouter} from 'react-router-dom';
 import otrok from './img/otrok.jpg';
 import valentina from './img/Valentina.jpg';
 import andrej from './img/Andrej.jpg';
@@ -7,9 +7,10 @@ import vasilij from './img/Vasilij.jpg';
 import serafim from './img/Serafim.jpg';
 import car from './img/Car.jpg';
 
-export default function App() {
-  return (
-	<div className="wrap">
+const NewApp = (props) => {
+	return  <HashRouter basename={process.env.PUBLIC_URL}>
+		 <Provider store={store}>
+		 <div className="wrap">
 	<header className="header">
 		<div className="header__icon">
 			<img src={car} />
@@ -36,5 +37,38 @@ export default function App() {
 	</nav>
 	<Outlet />
 </div>
-  );
-}
+		 </Provider>
+	  </HashRouter>
+  }
+
+// // export default function App() {
+// //   return (
+// // 	<div className="wrap">
+// // 	<header className="header">
+// // 		<div className="header__icon">
+// // 			<img src={car} />
+// // 		</div>
+// // 		<div className="header__icon">
+// // 			<img src={andrej} />
+// // 		</div>
+// // 		<div className="header__icon">
+// // 			<img src={otrok} />
+// // 		</div>
+// // 		<div className="header__icon">
+// // 			<img src={valentina} />
+// // 		</div>
+// // 		<div className="header__icon">
+// // 			<img src={vasilij} />
+// // 		</div>
+// // 		<div className="header__icon">
+// // 			<img src={serafim} />
+// // 		</div>
+// // 	</header>
+// // 	<nav>
+// // 		<Link to="/invoices">Молитвы</Link> 
+// // 		<Link to="/expenses">Помяник</Link> 
+// // 	</nav>
+// // 	<Outlet />
+// // </div>
+// //   );
+// }
